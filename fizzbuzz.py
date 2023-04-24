c65f11370b017688ec9e18a507b7aac13086e7f6
CheongEun <ce090404@naver.com>
2023-04-24 21:37:41 +0900

    fect: Print fizz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,7 +6,12 @@ def do_fizzbuzz():
 	15: fizzbuzz
 	ect: num
 	"""
-	print('hello')
+
+	for i in range(1,11) :
+		if i%3 == 0 :
+			print("fizz")
+		else :
+			print(i)
 
 if __name__=='__main__':
 	do_fizzbuzz()
